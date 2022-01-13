91fd4ff084cb0d4083ec82bb27ef6f131012bcb0
Rodrigo Ferreira <ricksonk_f@yahoo.com.br>
2022-01-13 18:18:07 -0300

    upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Footer } from '../src/components/Footer/footer';
+import {GlobalStyle} from './global'
+import { Contact } from './components/Contact/contact';
+import { Pricing } from './components/Pricing/pricing';
+import { Customers } from '../src/components/Customers/customers'
+import { Gallery } from './components/Gallery/gallery';
+import { Collection } from './components/Collection/collection';
+import { Mission } from './components/Mission/mission';
+import { Header } from './components/Header/header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Mission />
+      <Collection />
+      <Gallery /> 
+      <Customers />
+      <Pricing />
+      <Contact />
+     <Footer />
+     <GlobalStyle/>
+
+     
     </div>
+    
   );
 }
 
